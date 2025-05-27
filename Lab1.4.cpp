4004cea8b20149f2de9c1974c26646f71e22c376
AnupCh37 <anup37@icloud.com>
2025-05-27 22:02:24 +0545

    Update Lab1.4.cpp

--- Lab1.4.cpp
+++ Lab1.4.cpp
@@ -1,4 +1,4 @@
-/*Write a program to read name, rollno, address, and phone number of each student
+/*Write a program in c to read name, rollno, address, and phone number of each student
 in your class using structure. Store the information in file so that you can
 recover the information later. While recovering the information from the file
 sort the information alphabetically according to the name.*/
