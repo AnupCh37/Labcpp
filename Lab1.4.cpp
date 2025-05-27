27d3a0477038b567e12df6f78d16f3b70a0d0e6f
AnupCh37 <anup37@icloud.com>
2025-05-27 22:01:43 +0545

    Add files via upload

---
+++ Lab1.4.cpp
@@ -0,0 +1,32 @@
+/*Write a program to read name, rollno, address, and phone number of each student
+in your class using structure. Store the information in file so that you can
+recover the information later. While recovering the information from the file
+sort the information alphabetically according to the name.*/
+#include<iostream>
+#include<stdlib.h>
+struct Student{
+char name[50];
+int rollno;
+int phno;
+char address[50];
+};
+int main(){
+struct Student s[48];
+int n,i;
+FILE *fp=fopen("student.txt","w");
+if(fp==NULL){
+printf("Error opening file:");
+exit(1);
+}
+printf("Enter the number of student: \n");
+scanf("%d",&n);
+printf("Enter the detail (Name/rollno/phno/address):\n");
+for(int i=0;i<n;i++){
+scanf("%s %d %d %s",s[i].name,&s[i].rollno,&s[i].phno,s[i].address);
+fwrite(&s,sizeof(Student),1,fp);
+}
+fclose(fp);
+return 0;
+}
+
+
