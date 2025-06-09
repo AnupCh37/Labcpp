bc5904919ade52f17f0a9df2f0894b0cf00ff24a
AnupCh37 <anup37@icloud.com>
2025-06-09 22:34:50 +0545

    Update lab3.4.cpp

--- lab3.4.cpp
+++ lab3.4.cpp
@@ -1,3 +1,4 @@
+//DSbaral
 /*Write a program with classes to represent a circle, rectangle, and triangle. Each class should have data members to represent the actual objects and member functions to read and display objects, find perimeter and area of the objects, and other useful functions. Use the classes to create objects in your program*/
 #include <iostream>
 #define PI 3.1414 
