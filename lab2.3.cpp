1fa86a872b3fccfb01a3780562b7aff0b1b143f5
AnupCh37 <anup37@icloud.com>
2025-06-16 21:11:15 +0545

    Update lab2.3.cpp

--- lab2.3.cpp
+++ lab2.3.cpp
@@ -1,6 +1,11 @@
 //DSbaral
-/*WAP in to Define two namespaces: Square and Cube . In both the namespaces, define an integer variable named "fun". The "fun" function in "Square" namespace, should return the square of an integer  passed as an argument while the "fun" function in "Cube"namespace ,should return the cube of an integer passed as the argument. In the main function ,set the integer varialbe "num" of the both the namespace with different values.Then, compute and print the cube of the integer variable"num" of the "Square namespace using the "fun" function of the "cube"namespace and the square of the integer vaiable "num" of the "Cube" namespace
-using the "fun" function of the "Square" namespace*/
+/*WAP in to Define two namespaces: Square and Cube . In both the namespaces, 
+define an integer variable named "fun". The "fun" function in "Square" namespace,
+should return the square of an integer  passed as an argument while the "fun" function in "Cube"namespace ,
+should return the cube of an integer passed as the argument. In the main function ,set the integer varialbe "num" 
+of the both the namespace with different values.Then, compute and print the cube of the integer variable"num" of
+the "Square namespace using the "fun" function of the "cube"namespace and the square of the integer vaiable "num" 
+of the "Cube" namespace using the "fun" function of the "Square" namespace*/
 #include <iostream>
 using namespace std;
 namespace Square
