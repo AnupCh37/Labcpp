4ac366c4f5d3cc08f6464d28073aa70dfdbd66dd
AnupCh37 <anup37@icloud.com>
2025-06-19 19:50:59 +0545

    Update lab2.4.cpp

--- lab2.4.cpp
+++ lab2.4.cpp
@@ -1,5 +1,6 @@
 //DSbaral
-/*WAP a function that passes two temperatures by reference and sets larger of the two numbers to a value entered by user by using return by reference */
+/*WAP a function that passes two temperatures by reference and 
+sets larger of the two numbers to a value entered by user by using return by reference */
 #include <iostream>
 using namespace std;
 float& greaterTemp(float &t1, float &t2)
