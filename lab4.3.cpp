192ed9d1e9184dbcd48525b4c347d2bbdcd72211
AnupCh37 <anup37@icloud.com>
2025-06-27 05:27:57 +0545

    Update lab4.3.cpp

--- lab4.3.cpp
+++ lab4.3.cpp
@@ -1,3 +1,4 @@
+//Dsbaral
 /* Write a class that can store Department ID and Department Name with constructors
 to initialize its members. Write destructor member in the same class and display the 
 message "Object n goes out of the scope". Your program should be made such that it 
